51fdc10460404218d9f0f625751d031ae5154709
Alumno <alumno@lan.esdmadrid.es>
2024-02-28 09:33:56 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", ()=>{
+    let gifContainer = document.querySelector(".gif");
+
+    let APIKey= "3TJbSgeGMATQSzlRKosduMHD37EGsKya"
+    let searchTerm = "dogs"
+
+    //form elements
+    let userName = document.querySelector("#userName")
+    let userPhone = document.querySelector("#userPhone")
+    let addNewUser = document.querySelector("#addUser")
+    let userList=[]
+    fetch(`https://65d5afe7f6967ba8e3bc37fa.mockapi.io/users`)
+        .then(res=> res.json())
+        .then(datos=> {
+            console.log(datos) 
+            userList = datos;
+          
+        })
+
+    const addNewUserFunc = ()=>{
+        let newUser = {
+            id : 23,
+            name : userName.value,
+            phone : userPhone.value  
+        }
+        fetch(`https://65d5afe7f6967ba8e3bc37fa.mockapi.io/users`, {
+            method : 'POST',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify(newUser)
+        }).then(res => res.json())
+        .then(data =>{
+            userList.push(newUser)
+        })
+    }
+    addNewUser.addEventListener("click", (e)=>{
+        e.preventDefault()
+        addNewUserFunc()
+    })
+})
